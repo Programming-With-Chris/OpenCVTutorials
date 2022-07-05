7b6f676aa68ef5162737fe6652c2e54c771d2e74
Programming-With-Chris <programmingwithchris91@gmail.com>
2022-07-05 18:22:55 -0500

    code from first video

---
+++ Video1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Emgu.CV; 
+
+
+namespace myNamespace 
+{
+  class Video1 
+  {
+    static void Main(string[] args)
+    {
+        Mat pic = new Mat(); 
+
+        pic = CvInvoke.Imread("./img/starrynight.jpg"); 
+
+        Mat gaussianBlur = new Mat(); 
+
+        CvInvoke.GaussianBlur(pic, gaussianBlur, new System.Drawing.Size(pic.Rows, pic.Cols), 7.0); 
+
+        CvInvoke.Imshow("starry night", pic); 
+        CvInvoke.Imshow("blurry night", gaussianBlur); 
+
+
+        CvInvoke.WaitKey(); 
+    }
+  }
+}
